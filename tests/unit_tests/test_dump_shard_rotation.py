8f9bb9f064de9e856f2717f194bda485c8528e3b
spoil-ed <zangxingawa@gmail.com>
2026-06-20 19:40:06 -0400

    test(collect): assert per-shard local demo indices in rotation test
    Signed-off-by: spoil-ed <zangxingawa@gmail.com>

--- tests/unit_tests/test_dump_shard_rotation.py
+++ tests/unit_tests/test_dump_shard_rotation.py
@@ -5,10 +5,12 @@ from dreamervla.workers.rollout import dump_worker as dw
 
 class _FakeWriter:
     created: list[str] = []
+    instances: list = []
     def __init__(self, reward_dir, hidden_dir, shard_name):
         self.shard_name = str(shard_name)
         self.demos: list[int] = []
         _FakeWriter.created.append(self.shard_name)
+        _FakeWriter.instances.append(self)
     def write_demo(self, index, steps, preprocess_config=None, data_attrs=None, **kw):
         self.demos.append(int(index))
     def close(self):
@@ -21,6 +23,7 @@ def _episode():
 
 def test_no_rotation_when_disabled():
     _FakeWriter.created = []
+    _FakeWriter.instances = []
     with mock.patch.object(dw, "RolloutDumpWriter", _FakeWriter):
         w = dw.RolloutDumpWorker("r", "h", demos_per_shard=0)
         w.init()
@@ -28,10 +31,12 @@ def test_no_rotation_when_disabled():
             w.add_episode(_episode())
         assert _FakeWriter.created == ["ray_shard_000.hdf5"]   # single shard
         assert w.size() == 5
+        assert _FakeWriter.instances[0].demos == [0, 1, 2, 3, 4]
 
 
 def test_rotates_every_n_demos():
     _FakeWriter.created = []
+    _FakeWriter.instances = []
     with mock.patch.object(dw, "RolloutDumpWriter", _FakeWriter):
         w = dw.RolloutDumpWorker("r", "h", demos_per_shard=2)
         w.init()
@@ -42,3 +47,4 @@ def test_rotates_every_n_demos():
             "ray_shard_000.hdf5", "ray_shard_001.hdf5", "ray_shard_002.hdf5"
         ]
         assert w.size() == 5
+        assert [w.demos for w in _FakeWriter.instances] == [[0, 1], [0, 1], [0]]
